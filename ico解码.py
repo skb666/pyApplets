ea2e967f1706d53abeadb1e88657906aa6b49785
skb666 <973689813@qq.com>
2020-03-13 17:23:41 +0800

    整理、添加、更新

--- ico解码.py
+++
@@ -1,6 +0,0 @@
-import base64,icon
-with open('icon.py') as f_obj:
-	for i in f_obj:
-		aa=i.split('=')
-		with open('%s.ico'%aa[0],'wb+') as ff:
-			ff.write(base64.b64decode(eval('icon.%s'%aa[0])))
