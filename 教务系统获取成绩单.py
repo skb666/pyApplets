b0b6af7756746c4027e07a2ebf0e2a56ea700da3
SKB <973689813@qq.com>
2021-08-26 17:48:36 +0800

    Update 教务系统获取成绩单.py

--- 教务系统获取成绩单.py
+++ 教务系统获取成绩单.py
@@ -6,7 +6,7 @@ import csv
 import re
 
 
-def getScores(username, password):
+def getScores(username, password, filename=None):
     #站点访问需要的信息
     website = {
         "publickey": {
@@ -45,31 +45,33 @@ def getScores(username, password):
             },
         },
         "scores": {
-            "url": f"https://jwxt.cjlu.edu.cn/cjcx/cjcx_cxXsKcList.html?gnmkdm=N305007&su={username}",
+            "url": f"https://jwxt.cjlu.edu.cn/cjcx/cjcx_cxDgXscj.html?doType=query&gnmkdm=N100801&su={username}",
             "headers": {
                 "Host": "jwxt.cjlu.edu.cn",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
                 "Accept": "application/json, text/javascript, */*; q=0.01",
                 "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
                 "Accept-Encoding": "gzip, deflate, br",
-                "Referer": f"https://jwxt.cjlu.edu.cn/cjcx/cjcx_cxDgXsxmcj.html?gnmkdm=N305007&layout=default&su={username}",
                 "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
                 "X-Requested-With": "XMLHttpRequest",
                 "Origin": "https://jwxt.cjlu.edu.cn",
                 "Connection": "keep-alive",
+                "Referer": f"https://jwxt.cjlu.edu.cn/xsxxxggl/xsgrxxwh_cxXsgrxx.html?gnmkdm=N100801&layout=default&su={username}",
                 "Sec-Fetch-Dest": "empty",
-                "Sec-Fetch-Mode": "no-cors",
+                "Sec-Fetch-Mode": "cors",
                 "Sec-Fetch-Site": "same-origin",
-                "Pragma": "no-cache",
-                "Cache-Control": "no-cache",
             },
             "data": {
+                "xh_id": username,
+                "xnm": "",
+                "xqm": "",
                 "_search": "false",
-                "nd": f"{str(time.time()).replace('.','')[:13]}",
-                "queryModel.showCount": "300",
+                "nd": str(time.time()).replace('.','')[:13],
+                "queryModel.showCount": "150",
+                "queryModel.sortName": "xqmmc",
                 "queryModel.currentPage": "1",
                 "queryModel.sortOrder": "asc",
-                "time": "1"
+                "time": "0"
             },
         },
     }
@@ -96,25 +98,29 @@ def getScores(username, password):
 
         #获取成绩明细
         response = session.post(website["scores"]["url"], headers=website["scores"]["headers"], data=website["scores"]["data"])
-
         #解析、存表
         if 200 == response.status_code:
             xf_z = 0
+            jd_avg = list()
             scores = response.json()['items']
-            with open(f"{username[-4:]}_{str(time.time())[4:10]}.csv", 'w', encoding='utf-8', newline='') as f_obj:
+            if not filename: filename = f"{username[-4:]}_{str(time.time())[4:10]}.csv"
+            with open(filename, 'w', encoding='utf-8', newline='') as f_obj:
                 csv_writer = csv.writer(f_obj)
-                csv_writer.writerow(["学年", "学期", "开课学院", "课程代码", "课程名称", "教学班", "学分", "总评"])
+                csv_writer.writerow(["学年", "学期", "课程代码", "课程名称", "学分", "成绩", "考试性质", "绩点", "课程性质"])
                 for score in scores:
-                    xf_z += float(score.get("xf", "0"))
+                    jd = float(score.get("jd", "0"))
+                    if jd:
+                        jd_avg.append(jd)
+                        xf_z += float(score.get("xf", "0"))
                     csv_writer.writerow([score.get("xnmmc", None), score.get("xqmmc", None), \
-                                         score.get('kkbmmc', None), score.get("kch", None), \
-                                         score.get("kcmc", None), score.get("jxbmc", None), \
-                                         score.get("xf", None), score.get("zpcj", None)])
-            print(f"获取成功 (^ v ^)\n\t{username}的总学分为： {xf_z}")
+                                         score.get('kch', None), score.get("kcmc", None), \
+                                         score.get("xf", None), score.get("cj", None), \
+                                         score.get("ksxz", None), score.get("jd", None), score.get("kcxzmc", None),])
+            print(f"成绩获取成功! (^ v ^)\n\t{username}\t->\t总学分: {xf_z}\t平均绩点: {sum(jd_avg)/len(jd_avg):.2f}\n")
         else:
-            print("成绩获取失败 (V ~ V)")
+            print(f"成绩获取失败! (V ~ V)\n\t{username}\n")
     except Exception as err:
-        print(f"出错啦！\n{err}")
+        print(f"出错啦！ (> ^ <)\n\t{err}\n")
 
     del session
 
@@ -122,8 +128,9 @@ def getScores(username, password):
 if __name__ == '__main__':
     #学号 | 密码
     account = [
-        ("1832331405", b"我的密码"),
+        ("1832331405", b"my password"),
+        ("1832331431", b"your password"),
     ]
 
     for username, password in account:
-        getScores(username, password)
+        getScores(username, password, f"{username}.csv")
